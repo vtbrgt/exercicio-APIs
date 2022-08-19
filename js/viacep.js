dcd3fafb8ef6e2167413e9d5d889ac07e4f93120
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-08-19 20:57:10 -0300

    dealing with fetched data

--- js/viacep.js
+++ js/viacep.js
@@ -1,18 +1,54 @@
 const input = document.querySelector('#cep');
 const btn = document.querySelector('button');
-let cepUser = 0;
 
-function getCEP(event) {
-  event.preventDefault();
-  cepUser = input.value;
-  console.log(cepUser);
-  promise(`https://viacep.com.br/ws/${cepUser}/json/`).then((cep) =>
-    console.log(cep)
-  );
-}
-
-btn.addEventListener('click', getCEP);
+let cepData = {}
+let logradouro
+let bairro
+let localidade
+let uf
+let resultado = []
 
 function promise(url) {
   return fetch(url).then((response) => response.json());
 }
+
+function setValues() {
+  logradouro = cepData.logradouro
+  bairro = cepData.bairro
+  localidade = cepData.localidade
+  uf = cepData.uf
+
+  resultado.push(logradouro, bairro, localidade, uf)
+  resultado = resultado.join(', ').toString()
+  console.log(resultado)
+}
+
+function getCEP(event) {
+  event.preventDefault();
+  let cepUser = input.value;
+  console.log(cepUser);
+  promise(`https://viacep.com.br/ws/${cepUser}/json/`)
+  .then((cep) => {
+    cepData = cep
+  })
+  .then(() => setValues())
+}
+
+function showData() {
+  getCEP(event)
+
+  const div = document.createElement('div')
+  const label = document.createElement('label')
+  const p = document.createElement('p')
+
+  label.innerText = 'Esse foi o resultado encontrado:';
+  p.innerText = resultado;
+
+  div.classList.add('container')
+  div.appendChild(label)
+  div.appendChild(p)
+  document.body.appendChild(div)
+}
+
+btn.addEventListener('click', showData);
+
