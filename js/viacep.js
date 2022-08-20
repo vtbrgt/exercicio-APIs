2ffd0b35fa9c992d05eb9d9a937bcbfe462f18d2
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-20 12:00:12 -0300

    updating classes

--- js/viacep.js
+++ js/viacep.js
@@ -1,26 +1,30 @@
 const input = document.querySelector('#cep');
 const btn = document.querySelector('button');
 
-let cepData = {}
-let logradouro
-let bairro
-let localidade
-let uf
-let resultado = []
+let cepData = {};
+let logradouro = '';
+let bairro = '';
+let localidade = '';
+let uf = '';
+let resultado = [];
+
+const p = document.createElement('p');
 
 function promise(url) {
   return fetch(url).then((response) => response.json());
 }
 
 function setValues() {
-  logradouro = cepData.logradouro
-  bairro = cepData.bairro
-  localidade = cepData.localidade
-  uf = cepData.uf
+  logradouro = cepData.logradouro;
+  bairro = cepData.bairro;
+  localidade = cepData.localidade;
+  uf = cepData.uf;
 
-  resultado.push(logradouro, bairro, localidade, uf)
-  resultado = resultado.join(', ').toString()
-  console.log(resultado)
+  resultado.push(logradouro, bairro, localidade, uf);
+  resultado = resultado.join(', ').toString();
+  console.log(resultado);
+
+  p.innerText = resultado;
 }
 
 function getCEP(event) {
@@ -28,27 +32,37 @@ function getCEP(event) {
   let cepUser = input.value;
   console.log(cepUser);
   promise(`https://viacep.com.br/ws/${cepUser}/json/`)
-  .then((cep) => {
-    cepData = cep
-  })
-  .then(() => setValues())
+    .then((cep) => {
+      cepData = cep;
+    })
+    .then(() => setValues());
 }
 
 function showData() {
-  getCEP(event)
+  getCEP(event);
 
-  const div = document.createElement('div')
-  const label = document.createElement('label')
-  const p = document.createElement('p')
+  const div = document.createElement('div');
+  const label = document.createElement('label');
 
   label.innerText = 'Esse foi o resultado encontrado:';
-  p.innerText = resultado;
 
-  div.classList.add('container')
-  div.appendChild(label)
-  div.appendChild(p)
-  document.body.appendChild(div)
+  div.classList.add('container');
+  div.appendChild(label);
+  div.appendChild(p);
+
+  const verifica = document.querySelector('div');
+  console.log(verifica);
+  if (verifica == null) {
+    console.log('teste');
+    document.body.appendChild(div);
+  }
+
+  cepData = {};
+  logradouro = '';
+  bairro = '';
+  localidade = '';
+  uf = '';
+  resultado = [];
 }
 
 btn.addEventListener('click', showData);
-
