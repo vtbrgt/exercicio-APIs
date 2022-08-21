74858adbfe043a35a4c24e9daeeb96ea174b138f
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-08-20 22:17:27 -0300

    blockchain finished

--- js/blockchain.js
+++ js/blockchain.js
@@ -1,7 +1,3 @@
-// Utilizando a API https://blockchain.info/ticker
-// retorne no DOM o valor de compra da bitcoin and reais.
-// atualize este valor a cada 30s
-
 const span = document.querySelector('span')
 let valorReais = {}
 
@@ -9,7 +5,10 @@ function getValue() {
     fetch('https://blockchain.info/ticker')
     .then(response => response.json())
     .then(BRL => valorReais = BRL.BRL)
-    .then(set => span.innerText = valorReais.last)
+    .then((set) => {
+       span.innerText = ('R$ ' + valorReais.buy).replace('.', ',') 
+    }
+    )
 }
 getValue()
 setInterval(getValue, 30000)
